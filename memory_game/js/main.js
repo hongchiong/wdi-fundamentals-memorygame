ac6f668b6877f4d8eea76a53770dc1d56dc56dda
hongchiong <hongchiong@gmail.com>
2018-11-16 17:55:21 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,22 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
   } else {
-    alert("Sorry, try again.");
+    alert("Please select exactly 2 cards");
   }
-} else {
-  console.log("Please select exactly 2 cards");
 }
+function flipCard(cardId) {
+  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId]);
+  checkForMatch();
+};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+flipCard(0);
+flipCard(2);
